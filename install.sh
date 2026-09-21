c375a78069f5b534767f1277ce7965bb663b53f4
user-root <usersolo1@protonmail.com>
2026-09-21 21:40:09 +0000

    Update the create file and cp configuration

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 # Create the config folder if it doesn't exist
-#mkdir -p "$HOME/.zsh-hooks/zsh-jump"
-#print -n "\n [*] Create the dir '.zsh-hooks/zsh-jump' for the tool \n"
+mkdir -p "$HOME/.zsh-hooks/zsh-jump"
+print -n "\n [*] Create the dir '.zsh-hooks/zsh-jump' for the tool \n"
 # Copy files
-#cp -r .zsh-hooks/zsh-jump/* "$HOME/.zsh-hooks/zsh-jump/"
-#print -n "\n [*] Copying files from 'Repo/.zsh-hooks/zsh-jump' to '$HOME/.zsh-hooks/zsh-jump' for the tool \n"
+cp -r .zsh-hooks/zsh-jump/* "$HOME/.zsh-hooks/zsh-jump/"
+print -n "\n [*] Copying files from 'Repo/.zsh-hooks/zsh-jump' to '$HOME/.zsh-hooks/zsh-jump' for the tool \n"
 
 if [[ ! -s "$HOME/.chpwd-recent-dirs" ]]; then 
 	touch "$HOME/.chpwd-recent-dirs"
