a8d6faee7fbb9c849fe7c099ec83812fd20cb14a
user-root <usersolo1@protonmail.com>
2026-09-21 20:05:13 +0000

    Add the intall script fo the tool

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+# Create the config folder if it doesn't exist
+mkdir -p "$HOME/.zsh-hooks/zsh-jump"
+print -n "\n [*] Create the dir '.zsh-hooks/zsh-jump' for the tool \n"
+# Copy files
+cp -r .zsh-hooks/zsh-jump/* "$HOME/.zsh-hooks/zsh-jump/"
+# Copy .zsh configuration
+cat .zshrc >> "$HOME/.zshrc"
+print "Installation complete!"
